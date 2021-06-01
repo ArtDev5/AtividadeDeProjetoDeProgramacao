2ef869a55ea7722abcb30127ec596d2ed0153aa4
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-05-31 22:49:41 -0300

    Iniciando spring boot

---
+++ ProjetoDeProg/src/main/java/ApiRest/ProjetoDeProg/ProjetoDeProgApplication.java
@@ -0,0 +1,13 @@
+package ApiRest.ProjetoDeProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeProgApplication.class, args);
+	}
+
+}
---
+++ ProjetoDeProg/src/test/java/ApiRest/ProjetoDeProg/ProjetoDeProgApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiRest.ProjetoDeProg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeProgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
