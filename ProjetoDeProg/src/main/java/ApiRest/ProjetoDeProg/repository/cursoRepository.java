d4d7ddadcd92c10dcdea2a727300cd788e56d9bd
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-06-02 12:44:07 -0300

    Mudando o id para Integer

--- ProjetoDeProg/src/main/java/ApiRest/ProjetoDeProg/repository/cursoRepository.java
+++ ProjetoDeProg/src/main/java/ApiRest/ProjetoDeProg/repository/cursoRepository.java
@@ -3,9 +3,9 @@ package ApiRest.ProjetoDeProg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ApiRest.ProjetoDeProg.entity.Curso;
+import ApiRest.ProjetoDeProg.model.CursoEntity;
 
 @Repository
-public interface cursoRepository extends JpaRepository<Curso, Long>{
+public interface cursoRepository extends JpaRepository<CursoEntity, Integer>{
 
 }
